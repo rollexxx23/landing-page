97ac1784941a457f092e1cdf92f924f839330bc6
rollexxx23 <arinchatterjee00@gmail.com>
2023-10-17 18:20:44 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import AboutUs from "./components/aboutUs";
+import CTA from "./components/cta";
+import Footer from "./components/footer";
+import Headline from "./components/headline";
+import OurCraft from "./components/ourCraft";
+import TestimonialCard from "./components/testimonialCard";
+import Testimonials from "./components/testimonials";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="horizontal-margin-10 ">
+        <div className="flex items-center my-5 ">
+          <img src="images/logo.png" alt="Company Logo" />
+        </div>
+      </div>
+      <Headline />
+      <div className="horizontal-margin ">
+        <AboutUs />
+      </div>
+      <div className="h-80"></div>
+      <OurCraft />
+      <div className="h-60"></div>
+      <div className="horizontal-margin ">
+        <Testimonials />
+      </div>
+      <div className="h-40"></div>
+      <div className="horizontal-margin ">
+        <TestimonialCard />
+      </div>
+      <div className="h-80"></div>
+      <CTA />
+      <div className="h-80"></div>
+      <div className="horizontal-margin ">
+        <Footer />
+      </div>
+    </>
   );
 }
-
-export default App;
